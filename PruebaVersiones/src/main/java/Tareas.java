efb48b0abdaa48afde97c556c25e9f6b28fe1927
crrinconm <105617516+crrinconm@users.noreply.github.com>
2022-08-20 16:35:34 -0500

    Escribí en la línea 8

--- PruebaVersiones/src/main/java/Tareas.java
+++ PruebaVersiones/src/main/java/Tareas.java
@@ -5,6 +5,8 @@ public static void main (String args []){
     System.out.println ("Mi nombre es Pedro Emilio Caicedo Rios");
     System.out.println ("Aqui escribio Julanito de tal");
     System.out.println ("Aquí escribió Luis Alejandro Acosta");
+    System.out.println ("Aquí escribió Cristian Felipe Rincon Mendez");
+
 }
 
 }
