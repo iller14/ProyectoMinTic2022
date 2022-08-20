71e26c8639076de701c6444492084031e2b46134
granpeter <78057296+granpeter@users.noreply.github.com>
2022-08-20 12:14:16 -0500

    cambio a las 12:13 p.m

--- PruebaVersiones/src/main/java/Tareas.java
+++ PruebaVersiones/src/main/java/Tareas.java
@@ -4,7 +4,11 @@ public static void main (String args []){
 
     System.out.println ("Mi nombre es Pedro Emilio Caicedo Rios");
     System.out.println ("Aqui escribio Julanito de tal");
-    System.out.println("Aqui escribio Juanse");
+    System.out.println ("Aqui escribio Luis Alejandro Acosta");
+    System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:02 p.m");
+    System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:04 p.m");
+    System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:07 p.m");
+
 }
 
 }
