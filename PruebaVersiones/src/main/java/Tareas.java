eef1137149fb36b8209c69969e01ddb03ce9bfe2
JuanseG26 <sebas.galeano123@gmail.com>
2022-08-20 11:27:39 -0500

    Commit de prueba

--- PruebaVersiones/src/main/java/Tareas.java
+++ PruebaVersiones/src/main/java/Tareas.java
@@ -8,7 +8,7 @@ public static void main (String args []){
     System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:02 p.m");
     System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:04 p.m");
     System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:07 p.m");
-
+    System.out.println ("Aqui escribo para probar un commit and push el 20/08/2022 a las 12:13 p.m");
 }
 
 }
