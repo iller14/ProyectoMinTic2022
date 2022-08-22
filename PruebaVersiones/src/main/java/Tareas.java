4d31af1b85f49a11ccc8b4fa2f51fb3a5007d3a5
JuanseG26 <sebas.galeano123@gmail.com>
2022-08-22 18:31:23 -0500

    Actualización Nombres

--- PruebaVersiones/src/main/java/Tareas.java
+++ PruebaVersiones/src/main/java/Tareas.java
@@ -6,8 +6,9 @@ public static void main (String args []){
     System.out.println ("Aqui escribio Julanito de tal");
     System.out.println ("Aquí escribió Alejandro Acosta");
     System.out.println ("Aquí escribió Cristian Felipe Rincon Mendez");
-    System.out.println ("Aqui escribio el Domingo el Profe")
-    System.out.println ("Aqui escribio el Domingo Odaris")
+    System.out.println ("Aqui escribio el Domingo el Profe");
+    System.out.println ("Aqui escribio el Domingo Odaris");
+    System.out.println("Aqui escribio el lunes Juanse");
 }
 
 }
